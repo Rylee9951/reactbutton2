60f4e57205ace067b86b0aefd3c7417a6c6eba15
Ryan Lee <djrlee51@gmail.com>
2020-06-05 10:12:10 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state={
+    counter: 0,
+    like: 'likes'
+  }
+  handleClick = (e) =>{
+    if(this.state.counter === 0){
+      this.setState({
+        like: 'like'
+      })
+    }else{
+      this.setState({
+        like: 'likes'
+      })
+    }
+    this.setState({
+      counter: this.state.counter + 1
+    })
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+       <button onClick={this.handleClick}>{this.state.counter} {this.state.like}</button>
       </div>
     );
   }
